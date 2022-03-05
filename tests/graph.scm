0a4d2b2ad428fb068405a3c1f2e5c55786717a47
Siyuan Chen <chansey97@outlook.com>
2022-03-06 04:22:13 +0800

    Add tests.
    graph.scm is from ../graph.scm

---
+++ tests/graph.scm
@@ -0,0 +1,29 @@
+(load "require.scm")
+
+;; a -> b -> c -> a
+
+(define edge
+  (lambda (in-node out-node)
+    (conde
+      [(== 'a in-node)
+       (== 'b out-node)]
+      [(== 'b in-node)
+       (== 'c out-node)]
+      [(== 'c in-node)
+       (== 'a out-node)]
+      )))
+
+(define tabled-path
+  (tabled (in out p)
+    (conde
+      [(== in out) (== '() p)]
+      [(fresh (node p^)
+         (== (cons (list in node) p^) p)
+         (edge in node)
+         (tabled-path node out p^))])))
+
+(run 3 (p)
+  (tabled-path 'a 'c p))
+;; (((a b) (b c) (c a) (a b) (b c) (c a) (a b) (b c))
+;;  ((a b) (b c) (c a) (a b) (b c))
+;;  ((a b) (b c)))
